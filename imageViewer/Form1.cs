e65896722250fd4873569c0e4762b53b3cdc82ac
Heesu Yang <hsge0602@gmail.com>
2019-06-13 11:08:44 +0900

    #1 이미지 리스트에 이미지 추가

--- imageViewer/Form1.cs
+++ imageViewer/Form1.cs
@@ -11,9 +11,14 @@ namespace imageViewer
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
         }
     }
 }
