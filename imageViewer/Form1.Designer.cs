69a05ae2ca281f734a581067858274a9558ceb9d
Heesu Yang <hsge0602@gmail.com>
2019-06-13 10:44:08 +0900

    초기 커밋.

---
+++ imageViewer/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace imageViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImg = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblNum = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImg
+            // 
+            this.picImg.Location = new System.Drawing.Point(12, 12);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(256, 144);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImg.TabIndex = 0;
+            this.picImg.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(193, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblNum
+            // 
+            this.lblNum.Font = new System.Drawing.Font("굴림", 14F);
+            this.lblNum.Location = new System.Drawing.Point(103, 162);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(100, 23);
+            this.lblNum.TabIndex = 3;
+            this.lblNum.Text = "/";
+            this.lblNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 279);
+            this.Controls.Add(this.lblNum);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picImg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "사진첩";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblNum;
+    }
+}
+
