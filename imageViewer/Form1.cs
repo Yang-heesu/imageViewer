1669ed0812cbc8f4d478b801f90ea252a13fd775
Heesu Yang <hsge0602@gmail.com>
2019-06-13 11:34:47 +0900

    Revert "#2 이미지 출력, 이전보기/다음보기 버튼 소스 구현"
    This reverts commit fdcd3a3aa55d274ce4ebc2fdd22bd91bc9a1ec51.

--- imageViewer/Form1.cs
+++ imageViewer/Form1.cs
@@ -11,7 +11,6 @@ namespace imageViewer
 {
     public partial class Form1 : Form
     {
-        int imgCount = 0;
 
         public Form1()
         {
@@ -20,35 +19,6 @@ namespace imageViewer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            ShowImage();
-        }
-
-        private void ShowImage()
-        {
-            this.picImg.Image = this.imgList.Images[this.imgCount];
-            this.lblNum.Text = (this.imgCount + 1) + "/" + this.imgList.Images.Count;
-        }
-
-        private void BtnPrev_Click(object sender, EventArgs e)
-        {
-            this.imgCount--;
-            if (this.imgCount < 0)
-            {
-                this.imgCount = this.imgList.Images.Count-1;
-                //this.imgList.Images.Count = 43
-                //배열은 0~42만 존재함
-            }
-            ShowImage();
-        }
-
-        private void BtnNext_Click(object sender, EventArgs e)
-        {
-            this.imgCount++;
-            if (this.imgCount >= this.imgList.Images.Count)
-            {
-                this.imgCount = 0;
-            }
-            ShowImage();
         }
     }
 }
