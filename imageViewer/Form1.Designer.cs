21a52d2701eb0985f2a4a4352878c89a80540953
Heesu Yang <hsge0602@gmail.com>
2019-06-13 10:51:04 +0900

    #1 이미지뷰, 이미지 리스트, 버튼, 라벨 폼 구현

--- imageViewer/Form1.Designer.cs
+++ imageViewer/Form1.Designer.cs
@@ -28,10 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picImg = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
             this.lblNum = new System.Windows.Forms.Label();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.btnNext = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,42 +46,48 @@
             this.picImg.TabIndex = 0;
             this.picImg.TabStop = false;
             // 
-            // button1
+            // btnPrev
             // 
-            this.button1.Location = new System.Drawing.Point(12, 162);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(193, 162);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnPrev.Location = new System.Drawing.Point(12, 168);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(85, 38);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "이전";
+            this.btnPrev.UseVisualStyleBackColor = true;
             // 
             // lblNum
             // 
             this.lblNum.Font = new System.Drawing.Font("굴림", 14F);
-            this.lblNum.Location = new System.Drawing.Point(103, 162);
+            this.lblNum.Location = new System.Drawing.Point(103, 168);
             this.lblNum.Name = "lblNum";
-            this.lblNum.Size = new System.Drawing.Size(100, 23);
+            this.lblNum.Size = new System.Drawing.Size(74, 38);
             this.lblNum.TabIndex = 3;
             this.lblNum.Text = "/";
             this.lblNum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // imgList
+            // 
+            this.imgList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imgList.ImageSize = new System.Drawing.Size(256, 144);
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(183, 168);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(85, 38);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "다음";
+            this.btnNext.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(283, 279);
+            this.ClientSize = new System.Drawing.Size(283, 217);
+            this.Controls.Add(this.btnNext);
             this.Controls.Add(this.lblNum);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.picImg);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -93,9 +101,10 @@
         #endregion
 
         private System.Windows.Forms.PictureBox picImg;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPrev;
         private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
